af491b7c7f2c097389f6675eecbb0aea25564fc9
danasselin <dasselin86@gmail.com>
2019-09-19 23:40:18 -0400

    Get modal nav working

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link, StaticQuery, graphql } from 'gatsby';
+
+const PostPreview = ({ postData, classes }) => (
+  <div className={`nav-more-posts-container sm-padding ${classes}`}>
+    <div class='card'>
+      <p className='bold uppercase'>posts</p>
+    </div>
+    <hr className='horizontal-divider' />
+    {postData &&
+      postData.map((post, i) => {
+        const horizontalDivider =
+          i !== postData.length - 1 ? (
+            <hr className='horizontal-divider' />
+          ) : null;
+        const postTitle = post.node.frontmatter.title;
+        const { slug } = post.node.fields;
+        return (
+          <Fragment>
+            <div className='card'>
+              <Link to={slug}>{postTitle}</Link>
+            </div>
+            {horizontalDivider}
+          </Fragment>
+        );
+      })}
+  </div>
+);
 
 const TabletSidebar = ({ postData, header, siteNavigation }) => {
   const dividerHtml = <p className='desert-sky-dusk capitalize'>|</p>;
@@ -28,117 +54,114 @@ const TabletSidebar = ({ postData, header, siteNavigation }) => {
         </div>
         <div className='icon-fox'></div>
       </div>
-      <div className='nav-more-posts-container sm-padding'>
-        <div class='card'>
-          <p className='bold uppercase'>posts</p>
-        </div>
-        <hr className='horizontal-divider' />
-        {postData &&
-          postData.map((post, i) => {
-            const horizontalDivider =
-              i !== postData.length - 1 ? (
-                <hr className='horizontal-divider' />
-              ) : null;
-            const postTitle = post.node.frontmatter.title;
-            const { slug } = post.node.fields;
-            return (
-              <Fragment>
-                <div className='card'>
-                  <Link to={slug}>{postTitle}</Link>
-                </div>
-                {horizontalDivider}
-              </Fragment>
-            );
-          })}
-      </div>
+      <PostPreview postData={postData} />
     </nav>
   );
 };
 
-const Layout = props => (
-  <StaticQuery
-    query={graphql`
-      query getLayoutData {
-        allMarkdownRemark(
-          sort: { fields: frontmatter___date, order: DESC }
-          limit: 3
-        ) {
-          edges {
-            node {
-              excerpt
-              fields {
+const Layout = props => {
+  const [isFooterNavActive, setFooterNavState] = useState(false);
+  return (
+    <StaticQuery
+      query={graphql`
+        query getLayoutData {
+          allMarkdownRemark(
+            sort: { fields: frontmatter___date, order: DESC }
+            limit: 3
+          ) {
+            edges {
+              node {
+                excerpt
+                fields {
+                  slug
+                }
+                frontmatter {
+                  date(formatString: "MMMM DD, YYYY")
+                  title
+                  description
+                }
+              }
+            }
+          }
+          site {
+            siteMetadata {
+              siteNavigation {
+                name
                 slug
               }
-              frontmatter {
-                date(formatString: "MMMM DD, YYYY")
-                title
-                description
-              }
             }
           }
         }
-        site {
-          siteMetadata {
-            siteNavigation {
-              name
-              slug
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      const { edges } = data.allMarkdownRemark;
-      const { siteNavigation } = data.site.siteMetadata;
-      const { location, title, children } = props;
-      const rootPath = `${__PATH_PREFIX__}/`;
-      let header;
+      `}
+      render={data => {
+        const { edges } = data.allMarkdownRemark;
+        const { siteNavigation } = data.site.siteMetadata;
+        const { location, title, children } = props;
+        const rootPath = `${__PATH_PREFIX__}/`;
+        let header;
 
-      if (location.pathname === rootPath) {
-        header = (
-          <h1>
-            <Link to={`/`} className='logo-text'>
-              {title}
-            </Link>
-          </h1>
+        if (location.pathname === rootPath) {
+          header = (
+            <h1>
+              <Link to={`/`} className='logo-text'>
+                {title}
+              </Link>
+            </h1>
+          );
+        } else {
+          header = (
+            <h3>
+              <Link to={`/`}>{title}</Link>
+            </h3>
+          );
+        }
+        const footerNavBtnHtml = (
+          <p
+            onClick={() => setFooterNavState(!isFooterNavActive)}
+            className={`bold italic${isFooterNavActive ? ' active' : ''}`}>
+            {isFooterNavActive ? 'hide posts' : 'show posts'}
+          </p>
         );
-      } else {
-        header = (
-          <h3>
-            <Link to={`/`}>{title}</Link>
-          </h3>
-        );
-      }
-      return (
-        <React.Fragment>
-          <div className='page-container'>
-            <div className='main-container flex-tablet'>
-              <main className='main-left'>{children}</main>
-              <TabletSidebar
-                postData={edges ? edges : null}
-                header={header}
-                siteNavigation={siteNavigation}
-              />
-            </div>
-            <footer className='footer'>
-              <div className='flex nav'>
-                <div className='flex nav-about-connect'>
-                  <p>about</p>
-                  <p>connect</p>
+        return (
+          <React.Fragment>
+            <div className='page-container'>
+              <div className='main-container flex-tablet'>
+                <main className='main-left'>{children}</main>
+                <TabletSidebar
+                  postData={edges ? edges : null}
+                  header={header}
+                  siteNavigation={siteNavigation}
+                />
+              </div>
+              <footer className='footer'>
+                <div
+                  className={`nav footer-nav-modal${
+                    isFooterNavActive ? ' active' : ''
+                  }`}>
+                  <div className='flex footer-nav-items-container'>
+                    <p>about</p>
+                    <p>connect</p>
+                    <p
+                      onClick={() => setFooterNavState(!isFooterNavActive)}
+                      className='bold italic'>
+                      {footerNavBtnHtml}
+                    </p>
+                  </div>
+                  <div className='modal-cover'></div>
                 </div>
-                <p className='bold italic'>more posts</p>
-              </div>
-              <div className='copyright-container'>
-                <p className='copyright-content'>{`copyright ${String.fromCharCode(
-                  169
-                )} 2019 foxdoubt`}</p>
-              </div>
-            </footer>
-          </div>
-        </React.Fragment>
-      );
-    }}
-  />
-);
+
+                <div className='copyright-container'>
+                  <p className='copyright-content'>{`copyright ${String.fromCharCode(
+                    169
+                  )} 2019 foxdoubt`}</p>
+                </div>
+              </footer>
+            </div>
+          </React.Fragment>
+        );
+      }}
+    />
+  );
+};
 
 export default Layout;
