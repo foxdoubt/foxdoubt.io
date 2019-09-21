b1f35495b0c00d77d9a81f1d8391965b59e36409
danasselin <dasselin86@gmail.com>
2019-09-20 21:20:01 -0400

    Get rid of next/prev page preview nav

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -7,7 +7,6 @@ import SEO from '../components/seo';
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark;
-    const { previous, next } = this.props.pageContext;
 
     return (
       <Layout location={this.props.location}>
@@ -31,32 +30,6 @@ class BlogPostTemplate extends React.Component {
           <section dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
         <hr className='horizontal-divider' />
-
-        <nav>
-          <ul
-            style={{
-              display: `flex`,
-              flexWrap: `wrap`,
-              justifyContent: `space-between`,
-              listStyle: `none`,
-              padding: 0
-            }}>
-            <li>
-              {previous && (
-                <Link to={previous.fields.slug} rel='prev'>
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={next.fields.slug} rel='next'>
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
-        </nav>
       </Layout>
     );
   }
