c24e7aab2f694b14e77cf8e0e82d48042b8eb367
danasselin <dasselin86@gmail.com>
2019-09-26 21:06:18 -0400

    Update manifest with foxdoubt icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,7 +143,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`
+        icon: `content/assets/homemade-foxdoubt.svg`
       }
     },
     `gatsby-plugin-offline`,
